75ab42233d7f87cf80a30c90821ee6cd3f1122a2
schedule9 <52425399+schedule9@users.noreply.github.com>
2021-05-30 22:17:46 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ def ANL_Build_Docker_Learning(deadline, learning_info):
     learning_str = learning_str.format(deadline)
     for (party, info) in learning_info:
         learning_str = learning_str + f"    - party: {party}" + "\n"
-        per_state, nego_data = info
-        learning_str = learning_str + "      parameters: {" + f"persistentstate: {per_state}, negotiationdata: ["
-        for nego_session in nego_data[:-1]:
-            learning_str = learning_str + f"{nego_session}, "
-        learning_str = learning_str + f"{nego_data[-1]}]"
-        learning_str = learning_str + "}\n"
+        # per_state, nego_data = info
+        # learning_str = learning_str + "      parameters: {" + f" persistentstate: {per_state}, negotiationdata: ["
+        # for nego_session in nego_data[:-1]:
+        #     learning_str = learning_str + f"{nego_session}, "
+        # learning_str = learning_str + f"{nego_data[-1]}]"
+        # learning_str = learning_str + "}\n"
     return learning_str
 
 
@@ -28,9 +28,10 @@ def ANL_Build_Docker_Negotiation(deadline, negotiation_info):
         negotiation_str = negotiation_str + f"    - party: {party}\n      profile: {profile}\n"
         if param is not None:
             per_state, nego_data = param
-            negotiation_str = negotiation_str + "      parameters: {" + f"persistentstate: {per_state}, negotiationdata: ["
-            negotiation_str = negotiation_str + f"{nego_data}]"
-            negotiation_str = negotiation_str + "}\n"
+            # negotiation_str = negotiation_str + "      parameters: {" + f"persistentstate: {per_state}, negotiationdata: ["
+            # negotiation_str = negotiation_str + f"{nego_data}]"
+            # negotiation_str = negotiation_str + "}\n"
+            negotiation_str += "      parameters: {}\n"
     return negotiation_str
 
 
@@ -53,7 +54,7 @@ def ANL_Build_Learn_Session(deadline, learning_session_cntr, learning_session_le
     return None
 
 
-def ANL_Build_Docker_Settings(docker_path="C:\\DEV\\Java\ANL\\DockerRunner", negotiation_deadline=3):
+def ANL_Build_Docker_Settings(docker_path="C:/Users/yairk/Documents/השכלה ולימודים/תואר שני/בינה מלאכותית מתקדם/ANL2021/Docker_runner", negotiation_deadline=3):
     parties_path = docker_path + "\\parties"
     profile_path = docker_path + "\\profiles"
     parties_files = glob.glob(parties_path + '\\*.jar')
@@ -65,7 +66,7 @@ def ANL_Build_Docker_Settings(docker_path="C:\\DEV\\Java\ANL\\DockerRunner", neg
     p2_learning_session_learn_cntr = 1
 
     total_scenarios_cnt = 0
-    with open('settings.yml', 'w') as writer:
+    with open('settings.yaml', 'w') as writer:
         for k, prof_path in enumerate(profile_paths):
             profile_files = glob.glob(prof_path + '\\*[0-9].json', recursive=False)
             if len(profile_files) == 0:
@@ -122,7 +123,7 @@ def ANL_Build_Docker_Settings(docker_path="C:\\DEV\\Java\ANL\\DockerRunner", neg
     print(total_scenarios_cnt)
 
 
-def ANL_Analyzer(anl_res_path='C:/DEV/Java/ANL/DockerRunner/results', show_all=False):
+def ANL_Analyzer(anl_res_path='C:/Users/yairk/Documents/השכלה ולימודים/תואר שני/בינה מלאכותית מתקדם/ANL2021/Docker_runner/results', show_all=True): 
     aul = {}
     neg_files = glob.glob(anl_res_path + '/*_negotiation.json')
     for f_p in neg_files:
@@ -165,7 +166,7 @@ def ANL_Analyzer(anl_res_path='C:/DEV/Java/ANL/DockerRunner/results', show_all=F
                         aul[key] = [0]
         if show_all:
             for key, val in ul.items():
-                plt.plot(np.array(val), label=m.group())
+                plt.plot(np.array(val), label=key)
             plt.legend(loc='upper left', borderaxespad=0.)
             plt.ylabel("Util")
             plt.title(f_p)
@@ -193,7 +194,7 @@ def ANL_Analyzer(anl_res_path='C:/DEV/Java/ANL/DockerRunner/results', show_all=F
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    ANL_Analyzer()
-    #ANL_Build_Docker_Settings()
+    # ANL_Analyzer()
+    ANL_Build_Docker_Settings()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
