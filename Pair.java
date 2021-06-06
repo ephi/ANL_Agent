1dff251a594970d78fcc1d00c798601943698038
schedule9 <52425399+schedule9@users.noreply.github.com>
2021-06-06 23:09:14 +0300

    Delete Pair.java

--- Pair.java
+++
@@ -1,8 +0,0 @@
-package geniusweb.exampleparties.learningagent;
-
-import java.util.HashMap;
-
-public class Pair {
-	public HashMap<String, Integer> vList;  
-	public int    					type = -1;//-1 - An invalid value type, 0 - Discrete value, 1 - Number value // TODO: handle continuous spaces
-}
