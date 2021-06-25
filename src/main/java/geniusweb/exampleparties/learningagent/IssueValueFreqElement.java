8201181210756d0a1e856c6e14669891c687450a
schedule9 <52425399+schedule9@users.noreply.github.com>
2021-06-25 18:35:10 +0300

    Delete IssueValueFreqElement.java

--- src/main/java/geniusweb/exampleparties/learningagent/IssueValueFreqElement.java
+++
@@ -1,7 +0,0 @@
-package geniusweb.exampleparties.learningagent;
-
-public class IssueValueFreqElement {
-	public String value;
-	public int    value_type = -1; //-1 - An invalid value type, 0 - Discrete value, 1 - Number value // TODO: in continuous bid space split to sections 
-	public int    freq;
-}
